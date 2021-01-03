e6d92c94fc16c1fd4f3d180ac7f34dc4341e08a5
Hasan <mhasan.kzl@gmail.com>
2021-01-03 21:16:27 +0300

    allow cors origin added

---
+++ src/main/java/com/example/library/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.example.library.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hi")
+    public String hi(){
+
+        return  "Hello youtube";
+    }
+
+}
